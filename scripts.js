e329100c8e125e88c7789f6e277c5bc0f8faeaa1
adriciachiarini <adricia.dev@gmail.com>
2025-07-08 18:05:21 -0300

    Página cover Nike

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+let body=document.querySelector("body")
+let tenis =document.querySelector(".imgtenis")
+
+function mudarVisual(cor, imagem){
+
+    tenis.classList.add("troca-efeito")
+    body.style.background= cor 
+  
+
+setTimeout(() => {
+    tenis.src = imagem
+    tenis.classList.remove ("troca-efeito") 
+}, 500);
+
+
+
+
+
+
+}
